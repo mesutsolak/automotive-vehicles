b9936d803f09941aff3b8cb7e2ab1589c2ddd467
Mesut <mesuttsolakk@gmail.com>
2023-11-14 22:41:01 +0300

    feat: validation lib added.

---
+++ src/libraries/validation/AutomotiveBrands.Lib.Validation/IOC/ServiceCollectionContainerBuilderExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace AutomotiveBrands.Lib.Validation.IOC
+{
+    public static class ServiceCollectionContainerBuilderExtensions
+    {
+        /// <summary>
+        /// Add fluent validation
+        /// </summary>
+        /// <param name="services">type of built-in service collection interface</param>
+        /// <seealso cref="https://docs.fluentvalidation.net/en/latest/"/>
+        /// <exception cref="ArgumentNullException">when the service provider cannot be built</exception>
+        /// <returns>type of built-in service collection interface</returns>
+        public static IServiceCollection AddValidation<TAssembyScanner>(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<TAssembyScanner>();
+            services.AddFluentValidationAutoValidation();
+            services.AddFluentValidationClientsideAdapters();
+
+            ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr-TR");
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add api behaviour options
+        /// </summary>
+        /// <remarks>web api projesine ve/veya controller'ına sahipseniz, ekleyiniz.</remarks>
+        /// <param name="services">type of built-in service collection interface</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.apibehavioroptions?view=aspnetcore-6.0"/>
+        /// <returns>type of built-in service collection interface</returns>
+        public static IServiceCollection AddApiBehaviorOptions(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressInferBindingSourcesForParameters = true;
+
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    IEnumerable<string> errors = context.ModelState.Values.Where(p => p.Errors.Any())
+                                                                          .SelectMany(p => p.Errors)
+                                                                          .Select(p => p.ErrorMessage);
+
+                    Log.Warning("-- Validation Error. Errors: {@errors} --", string.Join(",", errors));
+
+                    string error = errors.First();
+
+                    return new OkObjectResult(new
+                    {
+                        error
+                    });
+                };
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/libraries/validation/AutomotiveBrands.Lib.Validation/Infrastructure/Validators/AutomotiveBrandsApiValidator.cs
@@ -0,0 +1,66 @@
+﻿namespace AutomotiveBrands.Lib.Validation.Infrastructure.Validators
+{
+    public class AutomotiveBrandsApiValidator<TValidator> : AbstractValidator<TValidator>
+    {
+        protected bool ValidateChassis(string chassis)
+        {
+            if (string.IsNullOrWhiteSpace(chassis))
+                return false;
+
+            return RegularExpressions.Chassis.IsMatch(chassis);
+        }
+        protected bool ValidatePlate(string plate)
+        {
+            if (string.IsNullOrWhiteSpace(plate))
+                return false;
+
+            return RegularExpressions.Plate.IsMatch(plate);
+        }
+        protected bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            return RegularExpressions.Email.IsMatch(email);
+        }
+        protected bool ValidateAttachmentFile(byte[] attachmentFile)
+        {
+            return attachmentFile.Length > 0;
+        }
+        protected bool ValidateTcNumber(string tcNumber)
+        {
+            if (string.IsNullOrWhiteSpace(tcNumber))
+                return false;
+
+            return RegularExpressions.TcNumber.IsMatch(tcNumber);
+        }
+        protected bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return false;
+
+            return RegularExpressions.Password.IsMatch(password);
+        }
+        protected bool ValidateEmails(string contactFormAuthorizedMails)
+        {
+            if (!contactFormAuthorizedMails.Contains(';'))
+                return ValidateEmail(contactFormAuthorizedMails);
+
+            var emailAddresss = contactFormAuthorizedMails.Split(';');
+            foreach (var email in emailAddresss)
+            {
+                if (!ValidateEmail(email))
+                    return false;
+            }
+
+            return true;
+        }
+        protected bool ValidatePhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                return false;
+
+            return RegularExpressions.PhoneNumber.IsMatch(phoneNumber);
+        }
+    }
+}
