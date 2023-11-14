c277b11d31aa74ae9c152b3c5eab1a1d6791b138
Mesut <mesuttsolakk@gmail.com>
2023-11-14 22:49:32 +0300

    feat: api version lib added.

---
+++ src/libraries/apiversion/AutomotiveBrands.Lib.ApiVersion/Usings.cs
@@ -0,0 +1,9 @@
+global using AutomotiveBrands.Lib.ApiVersion.Infrastructure.Constants;
+global using AutomotiveBrands.Lib.ApiVersion.Infrastructure.Settings;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Versioning;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Options;
