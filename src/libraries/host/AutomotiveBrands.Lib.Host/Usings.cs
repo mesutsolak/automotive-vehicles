3683f275a75dd1e0ca09b60535df13ba79e3aeb8
Mesut <mesuttsolakk@gmail.com>
2023-11-14 22:59:58 +0300

    feat: host lib added.

---
+++ src/libraries/host/AutomotiveBrands.Lib.Host/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Serilog;
+global using System.Text.Json.Serialization;
