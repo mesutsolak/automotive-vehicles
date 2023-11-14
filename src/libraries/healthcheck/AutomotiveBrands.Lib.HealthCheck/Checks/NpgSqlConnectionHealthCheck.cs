ccee4d8c591b2d3050cc95ddab094488d7b706d7
Mesut <mesuttsolakk@gmail.com>
2023-11-14 23:56:00 +0300

    feat: rate limit and healthcheck libraries added.

---
+++ src/libraries/healthcheck/AutomotiveBrands.Lib.HealthCheck/Checks/NpgSqlConnectionHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿namespace AutomotiveBrands.Lib.HealthCheck.Checks
+{
+    internal static class NpgSqlConnectionHealthCheck
+    {
+        internal static IHealthChecksBuilder AddNpgSqlDatabaseChecks(this IHealthChecksBuilder healthChecksBuilder, IConfiguration configuration)
+        {
+            healthChecksBuilder.AddNpgSql(
+                    connectionString: "connection string",
+                    name: "[NPG SQL] - Yönetim Veri Tabanı",
+                    failureStatus: HealthStatus.Unhealthy,
+                    tags: new[] { "MSSQL Server", "ManagementDbContext", "Authentication" });
+
+            healthChecksBuilder.AddNpgSql(
+                  connectionString: "connection string",
+                  name: "[NPG SQL] - Lastik Veri Tabanı",
+                  failureStatus: HealthStatus.Unhealthy,
+                  tags: new[] { "MSSQL Server", "TyreShopDbContext", "TyreShopDb" });
+
+            return healthChecksBuilder;
+        }
+    }
+}
