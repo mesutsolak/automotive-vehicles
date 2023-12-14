23bf0c2efec26121b7b6c94efc6f965ea14ced04
Mesut <mesuttsolakk@gmail.com>
2023-12-14 22:48:58 +0300

    test: vehicle service test written.

---
+++ tests/services/vehicle/AutomotiveBrands.VehicleService.Test/Infrastructure/ControllerFixture.cs
@@ -0,0 +1,17 @@
+﻿namespace AutomotiveBrands.VehicleService.Test.Infrastructure;
+
+[CollectionDefinition("AutomotiveBrands.VehicleService.Controllers")]
+public class ControllerFixtureCollection : ICollectionFixture<ControllerFixture>
+{
+}
+
+public sealed class ControllerFixture
+{
+    public void VerifyActionResult<T>(IActionResult result, bool isCheckForNull = true)
+    {
+        var responseModel = Assert.IsAssignableFrom<T>(result);
+        if (isCheckForNull)
+            Assert.NotNull(responseModel);
+        Assert.NotNull(responseModel);
+    }
+}
